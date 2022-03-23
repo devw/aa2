6f5c1673ce6cd958ebcd9b595b81f7eeecd2c2f1
Antonio Pierro <antonio.pierro@gmail.com>
2022-03-23 11:32:24 +0100

    feat: remove solution

--- server/tree.py
+++ server/tree.py
@@ -5,34 +5,15 @@ import json
 class TreeNode(dict):
     def __init__(self, name):
         self.__dict__ = self
-        self.name = name
-        self.parent = None
-        self.children = []
+        # to complete
 
     def add_child(self, child):
-        child.parent = self.name
-        self.children.append(child)
+        # to complete
+        pass
 
 def build_product_tree():
     root = TreeNode("Animal")
-
-    mammel = TreeNode("Mammals")
-    mammel.add_child(TreeNode("Dog"))
-    mammel.add_child(TreeNode("Cat"))
-    mammel.add_child(TreeNode("Human"))
-
-    reptile = TreeNode("Reptiles")
-    reptile.add_child(TreeNode("Turtle"))
-    reptile.add_child(TreeNode("Snake"))
-    reptile.add_child(TreeNode("Crocodile"))
-
-    insects = TreeNode("Insects")
-    insects.add_child(TreeNode("Bee"))
-    insects.add_child(TreeNode("Fly"))
-
-    root.add_child(mammel)
-    root.add_child(reptile)
-    root.add_child(insects)
+    # to complete
   
     return root
 
@@ -40,4 +21,3 @@ def build_product_tree():
 treeData = build_product_tree()
 
 tree = json.dumps([treeData])
-
